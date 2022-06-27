690f95995370f91d25240a85560214229148b0eb
Nicolas-ViniMateos <email@test.com>
2022-06-27 10:56:13 -0300

    Criacao do projeto

---
+++ src/TestaFuncionario.java
@@ -0,0 +1,15 @@
+
+public class TestaFuncionario {
+	public static void main(String[] args) 
+	{
+		Funcionario fun = new Funcionario();
+		
+		fun.cargo = "Eletricista";
+		fun.salario = 200.00;
+		
+		Funcionario fun2 = new Funcionario();
+		
+		fun2.cargo ="Cozinheiro";
+		fun2.salario = 100.00;
+	}
+}
---
+++ src/TestaTurma.java
@@ -0,0 +1,18 @@
+
+public class TestaTurma {
+	public static void main(String[] args) {
+		
+	
+	Turma turma = new Turma();
+	turma.periodo="Manhã";
+	turma.serie="1°";
+	turma.sigla="AI";
+	turma.tipo="Ensino Fundamental";
+	
+	Turma turma2 = new Turma();
+	turma2.periodo="Tarde";
+	turma2.serie="2°";
+	turma2.sigla="AE";
+	turma2.tipo="Ensino Médio";
+	}
+}
